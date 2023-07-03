aa49cc44465d0084d5f5e737908811d8dbb85b2a
BIROUE ISAAC <biroueisaac@gmail.com>
2023-07-03 13:19:03 +0100

    refactor my code

--- script.js
+++ script.js
@@ -12,31 +12,31 @@ const div = (operator1, operator2) => {
   return operator1 - operator2;
 };
 let operator1 = "";
-let  operator2 ="";
+let operator2 = "";
 let signe = "";
-let tableau=[]
-let display = document.querySelector('.first-row-element')
+let tableau = [];
+let display = document.querySelector(".first-row-element");
 let chiffre = document.querySelectorAll(".chiffre");
 function test(element) {
-    element.addEventListener("click", (element) => {
-      const digit = element.target.textContent
-      tableau.push(digit)
-      display.textContent=tableau.toString().replaceAll(',', '')
-    });
-  }
+  element.addEventListener("click", (element) => {
+    const digit = element.target.textContent;
+    tableau.push(digit);
+    display.textContent = tableau.toString().replaceAll(",", "");
+  });
+}
 chiffre.forEach(test);
-console.log(tableau)
+console.log(tableau);
 
 //erase the entire display
-const effacer = document.querySelector('.effacer')
-effacer.addEventListener('click',()=>{
-    display.textContent='0'
-    tableau=[]
-})
-//erase display digit per digit 
+const effacer = document.querySelector(".effacer");
+effacer.addEventListener("click", () => {
+  display.textContent = "0";
+  tableau = [];
+});
+//erase display digit per digit
 
-const supprimer = document.querySelector('.supprimer')
-supprimer.addEventListener('click',()=>{
-    tableau.pop()
-    display.textContent=tableau.toString().replaceAll(',','')
-})
+const supprimer = document.querySelector(".supprimer");
+supprimer.addEventListener("click", () => {
+  tableau.pop();
+  display.textContent = tableau.toString().replaceAll(",", "");
+});
